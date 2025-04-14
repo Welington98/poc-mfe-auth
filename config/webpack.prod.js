c73152e06316bda1a88f8a111267cb898d1361c5
welington98 <welingtonoliveira98@gmail.com>
2025-04-14 16:42:09 -0300

    update

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/auth/latest/',
+    publicPath: '/auth/',
   },
   plugins: [
     new ModuleFederationPlugin({

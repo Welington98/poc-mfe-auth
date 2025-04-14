52935111a3959358863ea30de94ae0a2edc71aaf
welington98 <welingtonoliveira98@gmail.com>
2025-04-14 17:32:03 -0300

    Atualiza o publicPath na configuração de produção do Webpack para o domínio correto

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -8,7 +8,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/',
+    publicPath: 'https://poc-mfe-auth.pages.dev/',
   },
   plugins: [
     new ModuleFederationPlugin({
